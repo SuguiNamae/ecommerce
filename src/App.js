b3cd767f6df021e9070bff4ef077f4f707e3a52b
suguinamae <rabin1381@fmail.com>
2024-03-21 14:37:01 -0800

    carousel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AdvertismentBar from './components/AdvertismentBar/AdvertismentBar';
 import Header from './components/header/header';
+import Carousel from './components/carousel/carousel';
 
 function App() {
   return (
     <div className="App">
       <AdvertismentBar/>
       <Header/>
+      <Carousel/>
     </div>
   );
 }
---
+++ src/components/carousel/carousel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./carousel.style.scss";
+const Carousel = () => {
+  return (
+    <div className="containall">
+      <img src="/assets/images/img_21_1944x.png" alt="" />
+    </div>
+  );
+};
+
+export default Carousel;

bc8193a4022a4b4cd9b114ffb787b006be57f31d
suguinamae <rabin1381@fmail.com>
2024-04-01 00:11:28 -0800

    contact us start

--- src/Router/Routing.js
+++ src/Router/Routing.js
@@ -7,7 +7,7 @@ import Home from "../pages/home/home";
 import About from "../pages/about/About";
 import NotFound from "../pages/NotFound/NotFound";
 import Sale from "../pages/sale/Sale";
-
+import ContactUs from "../pages/contactus/ContactUs";
 
 export const router = createBrowserRouter([
   {
@@ -21,6 +21,10 @@ export const router = createBrowserRouter([
   },
   {
     path: "/colections",
-    element: <Sale/>
-  }
+    element: <Sale />,
+  },
+  {
+    path: "/contact-us",
+    element: <ContactUs />,
+  },
 ]);
